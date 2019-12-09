ab868b4f629588f54f51a1bbf495ac8c6ca61133
ayoubkhan558 <ayoubkhan558@hotmail.com>
2019-12-09 23:58:24 +0500

    init

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+(function ($) {
+
+  "use strict";
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+    });
+
+
+    // HOME SLIDER & COURSES & CLIENTS
+    $('.home-slider').owlCarousel({
+      animateOut: 'fadeOut',
+      items:1,
+      loop:true,
+      dots:false,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+    })
+
+    $('.owl-client').owlCarousel({
+      animateOut: 'fadeOut',
+      loop: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        1000: {
+          items: 3,
+        }
+      }
+    });
+
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.custom-navbar a, #home a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
+
+})(jQuery);
